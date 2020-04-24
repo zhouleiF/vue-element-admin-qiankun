8187b04eb44357b573a61959874571a4bb6d1a05
周磊 <lei.zhou@ai-ways.com>
2020-04-24 15:26:28 +0800

    集成qiankun loadMicroApp api

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-Vue.use(Router)
-
 /* Layout */
 import Layout from '@/layout'
 
@@ -11,6 +9,8 @@ import componentsRouter from './modules/components'
 import chartsRouter from './modules/charts'
 import tableRouter from './modules/table'
 import nestedRouter from './modules/nested'
+
+Vue.use(Router)
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -119,6 +119,18 @@ export const constantRoutes = [
         component: () => import('@/views/profile/index'),
         name: 'Profile',
         meta: { title: 'Profile', icon: 'user', noCache: true }
+      }
+    ]
+  },
+  {
+    path: '/micro',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        component: () => import('@/views/MicroApp'),
+        name: 'MicroApp',
+        meta: { title: 'MicroApp', icon: 'user', noCache: true }
       }
     ]
   }
---
+++ src/views/MicroApp.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="container" />
+</template>
+
+<script>
+import { loadMicroApp } from 'qiankun'
+
+const microAppConfigs = [
+  {
+    name: 'sub7200',
+    entry: '//localhost:7200'
+  }
+]
+export default {
+  name: 'MicroApp',
+  data () {
+    return {
+      microApp: null
+    }
+  },
+  mounted () {
+    // const url = this.$route.path
+    // const app = microAppConfigs.find(e => ('/microApp' + e.url) === url)
+    // if (!app) this.$router.push('/')
+    const app = microAppConfigs[0]
+    this.microApp = loadMicroApp({
+      name: app.name,
+      entry: app.entry,
+      container: '#container'
+    })
+  },
+  beforeDestroy () {
+    this.microApp.unmount()
+  }
+}
+</script>
+
+<style>
+
+</style>
